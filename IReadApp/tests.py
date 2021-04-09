c6cc9e090e8297639c0d07b4eca1d3635609d42f
rica marie <ricamarie.paming@gsfe.tupcavite.edu.ph>
2021-04-09 16:54:31 +0800

    With Output Pass

--- IReadApp/tests.py
+++ IReadApp/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 
 class LogPageTest(TestCase):
-	def test_logpage_returns_correct_view(self):
+
+#	def test_logpage_returns_correct_view(self): #dito
+	def test_uses_mainpage_template(self):
 		response = self.client.get ('/')
 		self.assertTemplateUsed(response,'logpage.html')
+
+	def test_save_POST_request(self):
+		response = self.client.post('/', data={'AuthorEntry': 'NewAuthor'})
+		self.assertIn('NewAuthor', response.content.decode())
+		self.assertTemplateUsed(response,'logpage.html')
+
 
 
 # Create your tests here.
--- IReadApp/views.py
+++ IReadApp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 
 def LogPage(request):
-	return render(request,"logpage.html")
+	#if request.method == 'POST':
+	#	return HttpResponse(request.POST['AuthorEntry'])
+	#return render(request,"logpage.html")
+	return render(request,'logpage.html',{'NewAuthor': request.POST.get('AuthorEntry'), 'NewBook': request.POST.get('BookEntry'), 'NewGenre': request.POST.get('GenreEntry'), 'NewRemarks': request.POST.get('RemarksEntry',''),})
+
+
+
+
 # Create your views here.
